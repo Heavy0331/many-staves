baa0d2d1aca79afa27d0f8a013df976702960a52
Daniel Underwood <underthewood34@gmail.com>
2023-10-24 00:02:34 -0500

    add item group

--- src/main/java/com/heavy0331/manystaves/ManyStaves.java
+++ src/main/java/com/heavy0331/manystaves/ManyStaves.java
@@ -1,10 +1,12 @@
 package com.heavy0331.manystaves;
 
 import com.heavy0331.manystaves.item.ModItems;
+import com.heavy0331.manystaves.item.ModItemsGroup;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// TODO: Release in some form by version 0.1.0
 public class ManyStaves implements ModInitializer {
 	public static final String MOD_ID = "manystaves";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -12,6 +14,7 @@ public class ManyStaves implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("ManyStaves is initializing!");
+		ModItemsGroup.registerItemGroups();
 		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/heavy0331/manystaves/item/ModItemsGroup.java
@@ -0,0 +1,27 @@
+package com.heavy0331.manystaves.item;
+
+import com.heavy0331.manystaves.ManyStaves;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemsGroup {
+    public static final ItemGroup MANY_STAVES = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(ManyStaves.MOD_ID, "manystaves"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.manystaves"))
+                    .icon(() -> new ItemStack(ModItems.FIRE_STAFF)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.FIRE_STAFF);
+                        entries.add(ModItems.WATER_STAFF);
+                        entries.add(ModItems.PLANT_STAFF);
+
+                    }).build());
+
+
+    public static void registerItemGroups() {
+        ManyStaves.LOGGER.info("Registering item groups for " + ManyStaves.MOD_ID);
+    }
+}
