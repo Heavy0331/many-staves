f4be214a17ee8814ada8a7d6fe9012592273e0ad
Daniel Underwood <underthewood34@gmail.com>
2023-11-11 23:25:28 -0600

    Staff Wraith texture ftw

---
+++ src/client/java/net/heavy0331/manystaves/renderer/StaffWraithRenderer.java
@@ -0,0 +1,7 @@
+package net.heavy0331.manystaves.renderer;
+
+import net.heavy0331.manystaves.entity.StaffWraith;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+
+public class StaffWraithRenderer extends MobEntityRenderer<StaffWraith, > {
+}
