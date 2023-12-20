a820aae11670b3d63a7b48387b643be9e41496d5
Daniel Underwood <underthewood34@gmail.com>
2023-12-20 16:16:00 -0600

    add fireball behavior to fire staff

--- src/main/java/net/heavy0331/manystaves/item/FireStaff.java
+++ src/main/java/net/heavy0331/manystaves/item/FireStaff.java
@@ -1,20 +1,46 @@
 package net.heavy0331.manystaves.item;
 
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
+import net.minecraft.entity.projectile.SmallFireballEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class FireStaff extends MSItems {
-    public FireStaff(Item.Settings settings) {
+    public FireStaff(Settings settings) {
         super(settings);
     }
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+
+        // damage on use
+        ItemStack itemStack = playerEntity.getStackInHand(hand);
+        itemStack.damage(1, playerEntity, (p) -> p.sendToolBreakStatus(hand));
+
+        SmallFireballEntity smallFireball = new SmallFireballEntity(EntityType.SMALL_FIREBALL, world);
         playerEntity.playSound(SoundEvents.ENTITY_BLAZE_SHOOT, 1.0f, 1.0f);
+
+        // get player's pitch and yaw
+        float pitch = playerEntity.getPitch();
+        float yaw = playerEntity.getYaw();
+
+        // convert to vector
+        Vec3d direction = Vec3d.fromPolar(pitch, yaw);
+        smallFireball.updatePosition(
+                playerEntity.getX() + direction.x * 2.0D,
+                playerEntity.getY() + direction.y * 2.0D + playerEntity.getEyeHeight(playerEntity.getPose()),
+                playerEntity.getZ() + direction.z * 2.0D
+        );
+        smallFireball.setVelocity(direction.multiply(1.0D));
+
+        // set fireball owner to the current player
+        smallFireball.setOwner(playerEntity);
+
+        world.spawnEntity(smallFireball);
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
 }
--- src/main/java/net/heavy0331/manystaves/item/MSItems.java
+++ src/main/java/net/heavy0331/manystaves/item/MSItems.java
@@ -9,31 +9,25 @@ import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import org.jetbrains.annotations.NotNull;
-
-import static net.heavy0331.manystaves.entity.StaffWraith.STAFF_WRAITH;
-import static net.heavy0331.manystaves.entity.StaffWraith.registerSpawnEgg;
 
 public class MSItems extends Item {
 
     public MSItems(Settings settings) {
         super(settings);
     }
-    public static final Item FIRE_STAFF = registerItem("fire_staff", new FireStaff(new FabricItemSettings()));
+    public static final Item FIRE_STAFF = registerItem("fire_staff", new FireStaff(new FabricItemSettings().maxDamage(1500)));
     public static final Item WATER_STAFF = registerItem("water_staff", new Item(new FabricItemSettings()));
     public static final Item PLANT_STAFF = registerItem("plant_staff",new Item(new FabricItemSettings()));
     public static final Item FIRE_GEM = registerItem("fire_gem", new Item(new FabricItemSettings().fireproof()));
-    public static final Item STAFF_WRAITH_SPAWN_EGG = registerSpawnEgg("staff_wraith_spawn_egg", STAFF_WRAITH, 0x000000, 0xffffff, new Item.Settings());
 
 
 
     // @NotNull only serves here for documentation and could potentially break at a later point
-    private static void addItemsToCreativeTabGroup(@NotNull FabricItemGroupEntries entries) {
+    private static void addItemsToCreativeTabGroup(FabricItemGroupEntries entries) {
         entries.add(FIRE_STAFF);
         entries.add(WATER_STAFF);
         entries.add(PLANT_STAFF);
         entries.add(FIRE_GEM);
-        entries.add(STAFF_WRAITH_SPAWN_EGG);
     }
 
     public static Item registerItem(String name, Item item) {
