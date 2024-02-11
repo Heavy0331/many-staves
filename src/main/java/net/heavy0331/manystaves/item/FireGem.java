a27ba872402d31dee88e82fbdb79d1cd0a21fd82
Daniel Underwood <underthewood34@gmail.com>
2024-02-11 19:25:17 +0000

    Add Firegem functionality

--- src/main/java/net/heavy0331/manystaves/item/FireGem.java
+++ src/main/java/net/heavy0331/manystaves/item/FireGem.java
@@ -1,8 +1,19 @@
 package net.heavy0331.manystaves.item;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
 public class FireGem extends Item {
     public FireGem(Settings settings) {
         super(settings.fireproof());
     }
+    // Set the player on fire when held
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (selected) {
+            entity.setOnFireFor(1);
+        }
+    }
+    
 }
